9b6f7106526c073f4dbe3888f6da527411ad390a
jmmiguelberges-cell <jmmiguelberges@gmail.com>
2026-03-28 23:15:22 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,2 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
-export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
+
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: true,
+      storageKey: 'clocks-admin-auth',
+      autoRefreshToken: true,
+    }
+  }
+)
